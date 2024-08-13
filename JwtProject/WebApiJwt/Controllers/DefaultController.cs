5a15d38b92e1c1bd6e51e0635d41e21a897d0dd9
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-08-13 16:20:43 +0300

    jwt

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+
+namespace WebApiJwt.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DefaultController : ControllerBase
+{
+    [HttpGet("[action]")]
+    public IActionResult Test()
+    {
+        return Ok(new CreateToken().TokenCreate());
+    }
+
+    [HttpGet("[action]")]
+    public IActionResult AdminToken()
+    {
+        return Ok(new CreateToken().TokenCreateAdmin());
+    }
+
+    [Authorize]
+    [HttpGet("[action]")]
+    public IActionResult Test2()
+    {
+        return Ok("Test2");
+    }
+
+    [Authorize(Roles = "Admin,Visitor")]
+    [HttpGet("[action]")]
+    public IActionResult Test3()
+    {
+        return Ok("Test3");
+    }
+
+}
