ff1b526fe2debed03bf8ead1ee25f79432302359
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-08-16 12:25:08 +0300

    mail finish

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers;
+
+public class AdminMailController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(AdminMailViewModel model)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+
+        var mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "mveli5603@gmail.com");
+        mimeMessage.From.Add(mailboxAddressFrom);
+
+        var mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+        mimeMessage.To.Add(mailboxAddressTo);
+
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = model.Body;
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+        mimeMessage.Subject = model.Subject;
+
+
+        SmtpClient client = new SmtpClient();
+        client.Connect("smtp.gmail.com", 587, false);
+        client.Authenticate("mveli5603@gmail.com", "yvhhevfxutnfjhlu");
+        client.Send(mimeMessage);
+        client.Disconnect(true);
+        return View();
+
+        // 
+    }
+}
